a02332c87b295f0e7b33388786ed0fb0303f3cee
jinx <550961784@qq.com>
2022-03-18 15:24:28 +0800

    feat: ValidationPipe whitelist + forbidNonWhitelisted属性设置

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 允许接口请求的所有属性
+      forbidNonWhitelisted: true, // 如果请求参数没有在 whitelist 中定义，则抛出异常
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
