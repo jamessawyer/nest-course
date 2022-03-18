35d7c02a729107efe11e46380dd96fd6cc054a4c
jinx <550961784@qq.com>
2022-03-18 15:20:01 +0800

    feat: 使用ValidationPipe + mapped-types 对DTO数据进行验证

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,5 +1,13 @@
+import { IsString } from 'class-validator';
+
 export class CreateCoffeeDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly brand: string;
-  readonly flavor: string[];
+
+  // { each: true } 表示这是一个字符串数组
+  @IsString({ each: true })
+  readonly flavors: string[];
 }
