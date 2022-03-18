4e660ab096ed7cdd13d4cb6dc5177cfd48371b96
jinx <550961784@qq.com>
2022-03-18 14:18:40 +0800

    feat: 异常处理HttpException

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -17,7 +17,13 @@ export class CoffeesService {
   }
 
   findOne(id: string) {
-    return this.coffees.find((coffee) => coffee.id === +id);
+    const coffee = this.coffees.find((coffee) => coffee.id === +id);
+    if (!coffee) {
+      // throw new HttpException(`Coffee ${id} not found`, HttpStatus.NOT_FOUND);
+      // nest使用下面形式提供了封装
+      throw new NotFoundException(`Coffee ${id} not found`);
+    }
+    return coffee;
   }
 
   create(createCoffeeDto: any) {
