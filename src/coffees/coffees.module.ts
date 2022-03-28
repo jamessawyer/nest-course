c07a301e76291bb654d4b0a2f22e09928c585406
jinx <550961784@qq.com>
2022-03-28 11:32:15 +0800

    feat: Provider 非class注入方式

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from '../events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
@@ -9,6 +10,10 @@ import { Flavor } from './entities/flavor.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    { provide: COFFEE_BRANDS, useValue: ['Starbucks', 'Dunkin Donuts'] },
+  ],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
