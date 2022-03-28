e65d0350c13f7434e5bfe3439163fc0797ea61c6
jinx <550961784@qq.com>
2022-03-28 13:50:00 +0800

    feat: Async Provider 示例和应用场景

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
@@ -24,6 +25,20 @@ class ProductionConfigService {}
           : ProductionConfigService,
     },
     { provide: COFFEE_BRANDS, useValue: ['Starbucks', 'Dunkin Donuts'] },
+    // Async Providers
+    // 异步注入，比如以一些依赖需要从数据库中查询数据之后再加载时才能创建
+    // {
+    //   provide: COFFEE_BRANDS,
+    //   useFactory: () => async (connection: Connection) => {
+    //     // const coffeeBrands = await connection.query('SELECT * FROM xxx');
+    //     const coffeeBrands = await Promise.resolve([
+    //       'Starbucks',
+    //       'Dunkin Donuts',
+    //     ]);
+    //     return coffeeBrands;
+    //   },
+    //   inject: [Connection],
+    // },
   ],
   exports: [CoffeesService],
 })
