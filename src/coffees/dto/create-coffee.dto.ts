69f76895f352c580af1cbea8be1706331ae0fca5
jinx <550961784@qq.com>
2022-03-31 14:13:53 +0800

    feat: swagger - 使用ApiProperty给DTO对象添加注解,用于描述schema

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'coffee名字' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: 'coffee品牌' })
   @IsString()
   readonly brand: string;
 
+  @ApiProperty({ example: [] })
   // { each: true } 表示这是一个字符串数组
   @IsString({ each: true })
   readonly flavors: string[];
